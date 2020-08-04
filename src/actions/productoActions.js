213b615400a617b7385ffbb68cdf8346fafe0137
Enzo Orbe <enzoforbe@gmail.com>
2020-08-04 17:19:55 -0300

    Agregando nuevos productos y mostrando alertas

--- src/actions/productoActions.js
+++ src/actions/productoActions.js
@@ -3,11 +3,51 @@ import {
   AGREGAR_PRODUCTO_ERROR,
   AGREGAR_PRODUCTO_EXITO,
 } from "../types";
+import clienteAxios from "../config/axios";
+import Swal from "sweetalert2";
 
 //creear nuevos productos
 
 export function crearNuevoPorductoAction(producto) {
-  return () => {
-    console.log(producto);
+  return async (dispatch) => {
+    dispatch(agregarProducto());
+
+    try {
+      //insertar en la Api
+      await clienteAxios.post("/productos", producto);
+      //si todo sale bien actualizar el state
+      dispatch(agregarProductoExitoso(producto));
+      //alerta
+      Swal.fire("Correcto", "El producto se agrego correctamente", "success");
+    } catch (error) {
+      console.log(error);
+      //si hay un error cambiar el state
+      dispatch(agregarProductoError(true));
+
+      //alerta de error
+      Swal.fire({
+        icon: "error",
+        title: "Hubo un error",
+        text: "Hubo un error, intenta de nuevo",
+      });
+    }
   };
 }
+
+const agregarProducto = () => ({
+  type: AGREGAR_PRODUCTO,
+  payload: true,
+});
+
+//si el producto se guarda en la base de datos
+
+const agregarProductoExitoso = (producto) => ({
+  type: AGREGAR_PRODUCTO_EXITO,
+  payload: producto,
+});
+
+//si hubo un error
+const agregarProductoError = (estado) => ({
+  type: AGREGAR_PRODUCTO_ERROR,
+  payload: estado,
+});
--- src/components/NuevoProducto.js
+++ src/components/NuevoProducto.js
@@ -4,13 +4,17 @@ import { useDispatch, useSelector } from "react-redux";
 //actions de redux
 import { crearNuevoPorductoAction } from "../actions/productoActions.js";
 
-const NuevoProducto = () => {
+const NuevoProducto = ({ history }) => {
   //state del componente
   const [nombre, guardarNombre] = useState("");
   const [precio, guardarPrecio] = useState(0);
 
   //utilizar use dispatch y te crea una funcion
   const dispatch = useDispatch();
+
+  //acceder al state del error
+  const cargando = useSelector((state) => state.productos.loading);
+  const error = useSelector((state) => state.productos.error);
 
   //mandar a llamar el action de productoAction
   const agregarProducto = (producto) =>
@@ -31,6 +35,9 @@ const NuevoProducto = () => {
       nombre,
       precio,
     });
+
+    //redireccionar
+    history.push("/");
   };
 
   return (
@@ -72,6 +79,12 @@ const NuevoProducto = () => {
                 Agregar
               </button>
             </form>
+            {cargando ? <p>Cargando....</p> : null}
+            {error ? (
+              <p className="alert alert-danger p2 mt-4 text-center">
+                Hubo un error
+              </p>
+            ) : null}
           </div>
         </div>
       </div>
